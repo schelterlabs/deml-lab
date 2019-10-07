9572ed5e9c8a8219e92218d6a55dd9144c2dcb8f
TRokieG <hg1153@nyu.edu>
2019-10-06 23:37:34 -0400

    Update constraints.py

--- assignment1/components/constraints.py
+++ assignment1/components/constraints.py
@@ -6,41 +6,80 @@ class Constraint:
 
 class HasAtLeastNumRecords(Constraint):
     def __init__(self, num_records):
-        pass
+        self.num_records = num_records
 
-    # TODO Implement
+    def is_satisfied(self, dataframe):
+        if len(dataframe) >= self.num_records:
+            return True
+        else:
+            return False
+
 
 
 class NotNull(Constraint):
     def __init__(self, column):
-        pass
+        self.column = column
 
-    # TODO Implement
+    def is_satisfied(self, dataframe):
+        answer = dataframe[self.column].isnull()
+        for k in answer:
+            if k is True:
+                return False
+        return True
+
 
 
 class HasNumDistinctValues(Constraint):
     def __init__(self, column, min_distinct_values, max_distinct_values):
-        pass
+        self.column = column
+        self.min_distinct_values = min_distinct_values
+        self.max_distinct_values = max_distinct_values
 
-    # TODO Implement
+    def is_satisfied(self, dataframe):
+        data = dataframe[self.column].dropna()
+        num = len(data.unique())
+        if num >= self.min_distinct_values and num <= self.max_distinct_values:
+            return True
+
+        return False
 
 
 class IsInRange(Constraint):
     def __init__(self, column, min_value, max_value):
-        pass
+        self.column = column
+        self.min_value = min_value
+        self.max_value = max_value
 
-    # TODO Implement
+    def is_satisfied(self, dataframe):
+        min_n = dataframe[self.column].min()
+        max_n = dataframe[self.column].max()
+        if min_n >= self.min_value and max_n <= self.max_value:
+            return True
+        return False
+
+
 
 
 class And(Constraint):
     def __init__(self, *constraints):
-        pass
+        self.output = constraints
 
-    # TODO Implement
+
+
+    def is_satisfied(self, dataframe):
+        for obj in self.output:
+            if not obj.is_satisfied(dataframe):
+                return False
+        return True
 
 
 class Or(Constraint):
     def __init__(self, *constraints):
-        pass
+        self.output = constraints
 
-    # TODO Implement
+    def is_satisfied(self, dataframe):
+
+        for obj in self.output:
+            if obj.is_satisfied(dataframe):
+                return True
+        return False
